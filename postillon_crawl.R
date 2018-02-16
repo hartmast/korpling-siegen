1aa4b85d1fc8a9f7410b0ad7c65bf15b521217c0
Stefan Hartmann <hartmann_ste@gmx.de>
2018-02-16 12:03:00 +0100

    first commit

---
+++ postillon_crawl.R
@@ -0,0 +1,158 @@
+
+
+# Wir basteln uns ein Postillon-Korpus!
+# ---------------------------------------
+
+
+# Startseite des Postillon einlesen
+postillon <- scan("http://www.der-postillon.com/", # selbsterklärend
+                  what="character",                # was soll eingelesen werden? Textzeichen
+                  sep="\n")                        # Zeilenumbruch trennt die einzelnen sog. Felder
+
+# Seiten finden, auf die die Startseite verlinkt
+seiten <- grep("<a href=\'http://www.der-postillon.com/.*\\.html", 
+               postillon, 
+               value=T)
+
+# Die Teile, die nicht zum Link selbst gehören, entfernen
+seiten <- gsub("<a href=\'|\'>",    # ersetze diesen String...
+          "",                  # ... durch diesen String
+          seiten)              # ... und zwar in diesem Objekt.
+
+
+# den ersten Link einlesen
+post1 <- scan(seiten[1], what="character", sep="\n")
+
+# Die Inspektion des Seitenquelltexts zeigt, dass der eigentliche Text mit
+# "<div class='post-body entry-content post-body-item'" beginnt
+# und beim nächsten "</div> wieder endet.
+# Daher suchen wir zunächst "<div class='post-body entry-content post-body-item'"
+
+text_start <- grep("<div class='post-body entry-content post-body-item'", post1)
+
+# Jetzt suchen wir die "</div>"-Tags:
+div <- grep("^</div>", post1)
+
+
+# Davon brauchen wir den, der auf "div class='post-body entry-content..." etc. folgt -
+# also den ersten, dessen Zeilennummer größer ist als die, die wir als text_start gespeichert 
+# haben.
+text_end <- div[which(div>text_start)[1]]
+
+
+# Jetzt können wir uns den Text anschauen:
+post1[text_start:text_end]
+
+# ... und den relevanten Text in ein eigenes Objekt extrahieren.
+unser_text <- post1[text_start:text_end]
+
+
+# Nun entfernen wir alle Zeilen, die mit "<" beginnen und nicht zum eigentlichen
+# Text gehören.
+unser_text <- unser_text[-grep("^<", unser_text)]
+
+# Jetzt entfernen wir die "<br />" Tags, mit denen die Absatzumbrüche markiert sind.
+unser_text <- gsub("<br />", # wieder: ersetze diesen String...
+              " ",            # ... durch diesen String
+              unser_text)    # ... und zwar in diesem Objekt.
+
+
+# Als nächstes wollen wir den Text in einzelne Wörter aufsplitten.
+# Wir wollen aber, dass dabei die Satzzeichen abgetrennt sind.
+# Deshalb setzen wir vor allen nicht-alphanumerischen Zeichen ein Leerzeichen ein.
+# Dafür benutzen wir reguläre Ausdrücke: Das ^ bedeutet "nicht", [:alnum:] bedeutet
+# alphanumerisch.
+unser_text <- gsub("(?=[^[:alnum:]])",  # (?=) ist eine sog. lookahead assertion.
+                                        # Damit finden wir die Position VOR dem Zeichen nach dem =.
+                                        # so können wir "gsub", das eigentlich zum Ersetzen da ist,
+                                        # verwenden, ohne wirklich etwas zu ersetzen.
+                                        # Stattdessen fügen wir ein Leerzeichen vor dem relevanten
+                                        # Zeichen ein.
+     " ", 
+     unser_text, 
+     perl = T)
+
+# Jetzt splitten wir den Text auf, und zwar überall dort, wo Leerzeichen sind.
+unser_text <- unlist(strsplit(unser_text, " "))
+
+# Wir entfernen alle Elemente im Vektor, die keinen Text enthalten.
+unser_text <- unser_text[which(unser_text!="")]
+
+
+# Wir schauen uns das Ergebnis an...
+unser_text
+
+
+# ... und freuen uns!
+# Aber jetzt wollen wir das Ganze auch für die anderen Texte wiederholen.
+# Dafür benutzen wir einen Loop, in den wir alles das einbauen, was wir soeben gemacht haben.
+
+for(i in 2:length(seiten)) {
+  post1 <- scan(seiten[i], what="character", sep="\n")
+  
+  # Anfang des Textes finden
+  text_start <- grep("<div class='post-body entry-content post-body-item'", post1)
+  
+  # Ende des Textes finden
+  div <- grep("^</div>", post1)
+  text_end <- div[which(div>text_start)[1]]
+  
+  # Relevanten Text extrahieren
+  unser_text2 <- post1[text_start:text_end]
+  
+  
+  # Nun entfernen wir alle Zeilen, die mit "<" beginnen und nicht zum eigentlichen
+  # Text gehören.
+  unser_text2 <- unser_text2[-grep("^<", unser_text2)]
+  
+  # Absatzumbruch-Marker entfernen
+  unser_text2 <- gsub("<br />", # wieder: ersetze diesen String...
+                     " ",            # ... durch diesen String
+                     unser_text2)    # ... und zwar in diesem Objekt.
+  
+  # Leerzeichen vor Satzzeichen
+  unser_text2 <- gsub("(?=[^[:alnum:]])",
+                     " ", 
+                     unser_text2, 
+                     perl = T)
+  
+  # Text aufsplitten
+  unser_text2 <- unlist(strsplit(unser_text2, " "))
+  
+  # Leere Elemente entfernen
+  unser_text2 <- unser_text2[which(unser_text2!="")]
+  
+  # das Ganze an den bereits existierenden Text anhängen:
+  unser_text <- c(unser_text, unser_text2)
+  
+  # Fortschritt anzeigen
+  print(paste(i, " von ", length(seiten), " Seiten gecrawlt", sep="", collapse=""))
+  
+}
+
+
+# Jetzt speichern wir den Text in einer Textdatei.
+write.table(unser_text,        # das Objekt, das in die Datei geschrieben werden soll
+            "post1.txt",       # der Dateiname
+            quote=F,           # die einzelnen Wörter sollen nicht in Anführungszeichen stehen
+            row.names = F,     # keine Zeilennummerierung
+            col.names = F,     # keine Spaltennamen (sonst fügt er ganz oben ein "x" ein)
+            fileEncoding="UTF-8")
+
+
+
+# jetzt lassen wir den TreeTagger über das Ganze laufen
+# (in der Kommandozeile)
+
+# Befehle dafür (Pfadangaben bitte anpassen):
+# cd "/Users/stefanhartmann/Dropbox/Privat/Seminar WiSe201617/Siegen"
+# /Users/stefanhartmann/Downloads/cmd/tree-tagger-german post1.txt > post2.txt
+
+
+# jetzt lesen wir den Text wieder ein:
+unser_getaggter_text <- read.table("post2.txt", sep="\t", 
+                                   head=F, encoding="UTF-8", quote="", fill=T)
+
+unser_getaggter_text
+
+
